b7f9478f879610ca2806e0b324704d6be364b629
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-04-06 16:06:26 +0200

    feat: implement backend filtering on staff dashboard by date, time, and customer name; use dropdown for time with predefined hours (17:00 to 22:00)

--- staff/views.py
+++ staff/views.py
@@ -3,6 +3,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import ListView
 from bookings.models import Booking
 from datetime import date
+from django.db.models import Q
 
 # Admin check
 @method_decorator(user_passes_test(lambda u: u.is_staff), name='dispatch')
@@ -12,4 +13,20 @@ class BookingDashboardView(ListView):
     context_object_name = 'bookings'
 
     def get_queryset(self):
-        return Booking.objects.filter(date__gte=date.today()).order_by('date', 'time')
+        queryset = Booking.objects.filter(date__gte=date.today()).order_by('date', 'time')
+
+        # Get query parameters
+        selected_date = self.request.GET.get('date')
+        selected_time = self.request.GET.get('time')
+        customer = self.request.GET.get('customer')
+
+        if selected_date:
+            queryset = queryset.filter(date=selected_date)
+
+        if selected_time:
+            queryset = queryset.filter(time=selected_time)
+
+        if customer:
+            queryset = queryset.filter(user__username__icontains=customer)
+
+        return queryset
