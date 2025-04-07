d1bf5060c80219fa791021c070469b3370093271
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-04-07 13:53:46 +0200

    chore: configure email backend to console for development

--- minitaly/settings.py
+++ minitaly/settings.py
@@ -123,6 +123,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEFAULT_FROM_EMAIL = 'noreply@minitaly.com'  # Or anything you want displayed in dev
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
