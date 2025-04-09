b83b73bcb1f20b178d0f7397bef74d588509463a
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-04-09 10:12:32 +0200

    feat: add menu view to display categories and their menu items

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
+def menu_view(request):
+    # Groups menu items by category using Django’s prefetch_related
+    categories = Category.objects.prefetch_related('menu_items').all()
+    return render(request, 'menu/menu.html', {'categories': categories})
