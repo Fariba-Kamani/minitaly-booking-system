0cd614cfb15c78f940e31ee555555db7b39d75be
Fariba-Kamani <fariba.kamani87@gmail.com>
2025-04-11 17:51:18 +0200

    chore: move SECRET_KEY to environment variable

--- minitaly/settings.py
+++ minitaly/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#zfpy!yg++#x%e!l_elhdn+rd#kjy8okz7r5bh7+l)7l7c&8zs'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
